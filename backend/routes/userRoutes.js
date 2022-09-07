715d8a5c9aea5dd7bbaa61af4ec77a8a8e05fa8f
ED <granrr.z@gmail.com>
2022-09-07 21:03:21 +0900

    Backend setup completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser } = require("../controllers/userController")
+const {
+	registerUser,
+	loginUser,
+	getMe,
+} = require('../controllers/userController')
 
-router.post("/", registerUser)
-router.post("/login", loginUser)
+const { protect } = require('../middleware/authMiddleware')
+
+router.post('/', registerUser)
+router.post('/login', loginUser)
+router.get('/me', protect, getMe)
 
 module.exports = router
