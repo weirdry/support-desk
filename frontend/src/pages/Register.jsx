bb004b8e5267447d8f9e0046ad4d7af21fc3f54f
ED <granrr.z@gmail.com>
2022-09-07 23:42:34 +0900

    Redux setup initialised

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,113 @@
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { FaUser } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from '../features/auth/authSlice'
+
 export default function Register() {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: '',
+	})
+
+	const { name, email, password, password2 } = formData
+
+	const dispatch = useDispatch()
+
+	const { user, isLoading, isSuccess, message } = useSelector(
+		(state) => state.auth,
+	)
+
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}))
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+
+		if (password !== password2) {
+			toast.error('Passwords do not match')
+		} else {
+			const userData = {
+				name,
+				email,
+				password,
+			}
+
+			dispatch(register(userData))
+		}
+	}
+
 	return (
-		<div>
-			<h1>Register</h1>
-		</div>
+		<>
+			<section className="heading">
+				<h1>
+					<FaUser />
+					Register
+				</h1>
+				<p>Please create an account</p>
+			</section>
+
+			<section className="form">
+				<form onSubmit={onSubmit}>
+					<div className="form-group">
+						<input
+							type="text"
+							className="form-controll"
+							id="name"
+							name="name"
+							value={name}
+							onChange={onChange}
+							placeholder="Enter your name"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="email"
+							className="form-controll"
+							id="email"
+							name="email"
+							value={email}
+							onChange={onChange}
+							placeholder="Enter your email"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							className="form-controll"
+							id="password"
+							name="password"
+							value={password}
+							onChange={onChange}
+							placeholder="Enter password"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							className="form-controll"
+							id="password2"
+							name="password2"
+							value={password2}
+							onChange={onChange}
+							placeholder="Confirm password"
+							required
+						/>
+					</div>
+					<div className="form-group">
+						<button className="btn btn-block">Submit</button>
+					</div>
+				</form>
+			</section>
+		</>
 	)
 }
