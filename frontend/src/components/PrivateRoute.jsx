7ea11c60406422edebc630af13cf557c4f23632d
ED <granrr.z@gmail.com>
2022-09-09 18:35:35 +0900

    Prepare for deploy

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,15 +1,26 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import { useAuthStatus } from '../hooks/useAuthStatus'
-import Spinner from './Spinner'
+import { Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+// import { useAuthStatus } from '../hooks/useAuthStatus'
+// import Spinner from './Spinner'
 
-const PrivateRoute = () => {
-	const { loggedIn, checkingStatus } = useAuthStatus()
+// const PrivateRoute = () => {
+// 	const { loggedIn, checkingStatus } = useAuthStatus()
 
-	if (checkingStatus) {
-		return <Spinner />
-	}
+// 	if (checkingStatus) {
+// 		return <Spinner />
+// 	}
 
-	return loggedIn ? <Outlet /> : <Navigate to="/login" />
+// 	return loggedIn ? <Outlet /> : <Navigate to="/login" />
+// }
+
+// export default PrivateRoute
+
+const PrivateRoute = ({ children }) => {
+	const { user } = useSelector((state) => state.auth)
+
+	if (user) return children
+
+	return <Navigate to="/login" />
 }
 
 export default PrivateRoute
